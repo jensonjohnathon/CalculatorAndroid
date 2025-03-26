7a87188fb6c1f8825c62782c946dc7a7646a91e1
jensonjohnathon <johnathonreggentin@gmail.com>
2025-03-26 15:03:24 +0100

    Initial Commit no Logic just Buttons

---
+++ app/src/main/java/com/example/mobilesytemestarter/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mobilesytemestarter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button meinButton;
+
+    private TextView meinText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        meinButton = (Button) findViewById(R.id.ac);
+        meinText = (TextView) findViewById(R.id.textView);
+        meinText.setText("hello World");
+
+    }
+    public void winf(View v) {meinText.setText("Ich wurde geklickt");}
+}
